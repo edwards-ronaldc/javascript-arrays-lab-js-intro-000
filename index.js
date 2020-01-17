380c705e8d11b3d9e783d4397fddec5d7f5e0114
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-17 05:17:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-destructivelyAppendKitten(name){kittens.push(name); return kittens}
+function destructivelyAppendKitten(name){kittens.push(name); return kittens}
