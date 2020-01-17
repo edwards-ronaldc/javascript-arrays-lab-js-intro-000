e0832a359ad33778b3e45d4afb6290b61e1ee535
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-17 05:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyAppendKitten(name){kittens.push(name); return kittens}
 function destructivelyPrependKitten(name){kittens.unshift(name); return kittens}
 function destructivelyRemoveLastKitten(){kittens.pop(); return kittens}
 function destructivelyRemoveFirstKitten(){kittens.shift();return kittens}
-function appendKitten(name){return[...kittens, name]} 
+function appendKitten(name){return[...kittens, name]}
 function prependKitten(name){return[name, ...kittens]}
 function removeLastKitten(){kittens.slice(0,kittens.length-2)}
